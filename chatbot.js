d302b4008cd8a0dc759648a8ae949d27546148b1
ajgustmd <jhs0622@hanyang.ac.kr>
2023-10-02 21:56:22 +0900

    Added chatbot.js subcommand

--- chatbot.js
+++ chatbot.js
@@ -5,20 +5,34 @@ const openai = new OpenAI({
     apiKey : openaikey
 });
 
-const max_remeber_context = 5;
+max_remeber_context = 5;
 ai_context = [];
 ai_concept = "너는 '단또봇'라는 이름을 가진 디스코드에서 활동할 고양이 챗봇이야. 앞으로 하게 될 모든 대화에서 고양이 말투로 대답하면 돼. 앞으로 너가 할 모든 말끝에 '~다냥'을 붙여서 고양이 흉내를 내렴. 그리고 무슨 일이 있어도 말끝에 '다냥'을 붙이는 말투를 바꾸지 마. 조금 철없는 말투로 말하렴.";
 
+model = this.default_val.model;
+temperature = this.default_val.temperature;
+max_tokens = this.default_val.max_tokens;
+top_p = this.default_val.top_p;
+frequency_penalty = this.default_val.frequency_penalty;
+presence_penalty = this.default_val.presence_penalty;
+
 module.exports = {
+    default_val : {
+        model : "gpt-3.5-turbo",
+        temperature : 1,
+        max_tokens : 128,
+        top_p : 1,
+        frequency_penalty : 0,
+        presence_penalty : 0,
+    },
     async getResponce(content) {
         var input = {
-            model: "gpt-3.5-turbo",
-            //model: "gpt-4",
-            temperature: 1,
-            max_tokens: 128,
-            top_p: 1,
-            frequency_penalty: 0,
-            presence_penalty: 0,
+            model: model,
+            temperature: temperature,
+            max_tokens: max_tokens,
+            top_p: top_p,
+            frequency_penalty: frequency_penalty,
+            presence_penalty: presence_penalty,
             stop: ["]"],
         };
 
@@ -42,13 +56,48 @@ module.exports = {
         return responce.choices[0].message;
     },
 
-    setConcept(concept) {
-        ai_concept = concept;
-        console.log(ai_concept);
-    },
-
     clearContext() {
         ai_context = [];
-    }
-};
+    },
 
+    setModel(_model) {
+        model = _model;
+    },
+
+    setTemperature(_temperature) {
+        temperature = _temperature;
+    },
+
+    setMaxtokens(_max_tokens) {
+        max_tokens = _max_tokens;
+    },
+
+    setTop_p(_top_p) {
+        top_p = _top_p;
+    },
+
+    setFrequency_penalty(_frequency_penalty) {
+        frequency_penalty = _frequency_penalty;
+    },
+
+    setPresence_penalty(_presence_penalty) {
+        presence_penalty = _presence_penalty;
+    },
+
+    setConcept(_concept) {
+        ai_concept = _concept;
+    },
+
+    setMaxremember(_max_remember) {
+        max_remeber_context = _max_remember * 2 + 1;
+    },
+
+    initVal() {
+        model = this.default_val.model;
+        temperature = this.default_val.temperature;
+        max_tokens = this.default_val.max_tokens;
+        top_p = this.default_val.top_p;
+        frequency_penalty = this.default_val.frequency_penalty;
+        presence_penalty = this.default_val.presence_penalty;
+    },
+};
---
+++ commands/chatbot.js
@@ -0,0 +1,107 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('chatbot')
+        .setDescription('단또봇의 챗봇 기능을 조작합니다')
+        .addSubcommandGroup((group) => group.setName('set')
+            .setDescription('챗봇의 설정 값을 수정합니다')
+            .addSubcommand((subcommand) => subcommand.setName('model')
+                .setDescription('GPT 모델을 수정합니다')
+                .addStringOption((option) => option.setName('modelname')
+                    .setDescription('모델 이름')
+                    .setChoices(
+                        { name: "gpt-3.5-turbo", value: "gpt-3.5-turbo"}, // Default
+                        { name: "gpt-3.5-turbo-16k", value: "gpt-3.5-turbo-16k"},
+                        { name: "gpt-4", value: "gpt-4"})))
+            .addSubcommand((subcommand) => subcommand.setName('temperature') // 1
+                .setDescription('temperature 값을 수정합니다')
+                .addNumberOption((option) => option.setName('temperature')
+                    .setDescription('temperature')))
+            .addSubcommand((subcommand) => subcommand.setName('max_tokens') // 128
+                .setDescription('max_tokens 값을 수정합니다')
+                .addIntegerOption((option) => option.setName('max_tokens')
+                    .setDescription('max_tokens')))
+            .addSubcommand((subcommand) => subcommand.setName('top_p') // 1
+                .setDescription('top_p 값을 수정합니다')
+                .addNumberOption((option) => option.setName('top_p')
+                    .setDescription('top_p')))
+            .addSubcommand((subcommand) => subcommand.setName('Frequency penalty') // 0
+                .setDescription('Frequency penalty 값을 수정합니다')
+                .addNumberOption((option) => option.setName('Frequency penalty')
+                    .setDescription('Frequency penalty')))
+            .addSubcommand((subcommand) => subcommand.setName('Presence penalty') // 0
+                .setDescription('Presence penalty 값을 수정합니다')
+                .addNumberOption((option) => option.setName('Presence penalty')
+                    .setDescription('Presence penalty')))
+            .addSubcommand((subcommand) => subcommand.setName('concept')
+                .setDescription('AI의 컨셉을 수정합니다')
+                .addStringOption((option) => option.setName('concept')
+                    .setDescription('컨셉')
+                    .setRequired(true)))  
+            .addSubcommand((subcommand) => subcommand.setName('max_remember') // 2
+                .setDescription('기억할 수 있는 최대 대화 갯수를 수정합니다')
+                .addIntegerOption((option) => option.setName('max_remember')
+                    .setDescription('max_remember')))
+            .addSubcommand((subcommand) => subcommand.setName('init')
+                .setDescription('챗봇의 모델, temperature, max_tokens, top_p, Frequency penalty, Presence penalty 값을 기본값으로 초기화 합니다'))),
+        async execute(interaction) {
+            path = '../chatbot.js';
+            if (interaction.options.getSubcommandGroup() === 'set') {
+                sub = interaction.options.getSubcommand();
+                if (sub === 'model') {
+                    const { setModel } = require(path);
+                    model = interaction.options.getString('modelname');
+                    setModel(model);
+                    await interaction.reply('설정된 모델 : ' + model);
+                }
+                else if (sub === 'temperature') {
+                    const { setTemperature } = require(path);
+                    temperature = interaction.options.getNumber('temperature');
+                    setTemperature(temperature);
+                    await interaction.reply('설정된 temperature : ' + temperature);
+                }
+                else if (sub === 'max_tokens') {
+                    const { setMaxtokens } = require(path);
+                    max_tokens = interaction.options.getInteger('max_tokens');
+                    setMaxtokens(max_tokens);
+                    await interaction.reply('설정된 max_tokens : ' + max_tokens);
+                }
+                else if (sub === 'top_p') {
+                    const { setTop_p } = require(path);
+                    top_p = interaction.options.getNumber('top_p');
+                    setTop_p(top_p);
+                    await interaction.reply('설정된 top_p : ' + top_p);
+                }
+                else if (sub === 'Frequency penalty') {
+                    const { setFrequency_penalty } = require(path);
+                    frequency_penalty = interaction.options.getNumber('Frequency penalty');
+                    setFrequency_penalty(frequency_penalty);
+                    await interaction.reply('설정된 Frequency penalty : ' + frequency_penalty); 
+                }
+                else if (sub === 'Presence penalty') {
+                    const { setPresence_penalty } = require(path);
+                    presence_penalty = interaction.options.getNumber('Presence penalty');
+                    setPresence_penalty(presence_penalty);
+                    await interaction.reply('설정된 Presence penalty : ' + presence_penalty); 
+                }
+                else if (sub === 'concept') {
+                    const { setConcept } = require(path);
+                    concept = interaction.options.getString('concept');
+                    setConcept(concept);
+                    await interaction.reply('설정된 컨셉 : ' + concept);
+                }
+                else if (sub === 'max_remember') {
+                    const { setMaxremember } = require(path);
+                    max_remember = interaction.options.getInteger('max_remember');
+                    setMaxremember(max_remember);
+                    await interaction.reply('설정된 최대 기억 문맥 : ' + max_remember);
+                }
+                else if (sub === 'init') {
+                    const { initVal } = require(path);
+                    initVal();
+                    await interaction.reply('설정값이 초기화 되었습니다');
+                }
+            }
+        }
+}
